87d107860775ca18836c1155901054941933eff2
alessandro <alessandrohidalgo8@gmail.com>
2024-12-16 20:48:32 -0500

    solucion de la rutas

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: 'dist', // Carpeta donde se guarda el build
+  },
+  // Configuración para redirigir rutas a index.html
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
